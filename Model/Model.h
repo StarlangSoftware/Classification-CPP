200bff5513e60324c21fe37392ab0e175b133514
olcaytaner <olcaytaner@isikun.edu.tr>
2019-04-16 08:13:13 +0300

    Added serialization and deserialization

--- Model/Model.h
+++ Model/Model.h
@@ -10,6 +10,8 @@
 
 class Model {
 public:
+    Model() = default;
+    explicit Model(ifstream& inputFile) {};
     virtual string predict(Instance* instance) = 0;
     virtual void serialize(ostream &outputFile) = 0;
 };
